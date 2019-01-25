5435a1dc0f797b18f3bdb84ff12b730273c63aa0
Eric Olsen <eolsen@slc.mainstreamdata.com>
2019-01-25 10:35:14 -0700

    Remote Commit

--- src/Components/TwitterFeed/TwitterFeed.js
+++ src/Components/TwitterFeed/TwitterFeed.js
@@ -11,19 +11,41 @@ class TwitterFeed extends Component {
     render() {
         // Maps through a user's data returning timeline tweets & related media.
         const timeline = this.props.userTweetData.map( (tweet, i) => {
-          if(tweet.entities.media) {
-            return (
-                <div className='timeline-tweet' key={tweet.id}>
-                    <div className='tweet-text'>{tweet.text}</div>
-                    <img className='app-tweets-pics' src={tweet.entities.media[0].media_url} alt='tweet pic'/>
-                </div>
-            )
-          } 
-          return (    
-            <div className='timeline-tweet' key={tweet.id}>
-                <div className='tweet-text'>{tweet.text}</div>
-            </div>
-          )
+            if(tweet.extended_entities) {
+                if(tweet.extended_entities.media[0]) {
+                    if(tweet.extended_entities.media[0].video_info) {
+                        if(tweet.extended_entities.media[0].video_info.variants) {
+                            if (tweet.extended_entities.media[0].video_info.variants[0].url) {
+                                return (
+                                    <div className='timeline-tweet' key={tweet.id}>
+                                        <div className='tweet-text'>{tweet.text}</div>
+                    
+                                        {/* <video>
+                                            <source src={tweet.extended_entities.media[0].video_info.variants[0].url} type="video/mp4"/>
+                                        </video> */}
+
+                                        <video className='app-tweets-media' controls loop>
+                                        <source src={tweet.extended_entities.media[0].video_info.variants[0].url} type="video/mp4"/>
+                                        Your browser does not support the video tag.
+                                        </video>
+
+                                        {/* <iframe src={tweet.extended_entities.media[0].video_info.variants[0].url}></iframe> */}
+                    
+                    
+                                        {/* <img className='app-tweets-pics' src={tweet.entities.media[0].media_url} alt='tweet pic'/> */}
+                                    </div>
+                                )
+                            }
+                        }
+                    }
+                }
+            } else {
+              return (    
+                  <div className='timeline-tweet' key={tweet.id}>
+                      <div className='tweet-text'>{tweet.text}</div>
+                  </div>
+                )
+            }
         });
 
         return (
